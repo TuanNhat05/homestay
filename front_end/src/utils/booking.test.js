34cbc123f4c69f235cf9e221837bb924a481d48c
Homestay Deploy <homestay@deploy.local>
2026-08-09 16:25:17 +0700

    Update booking test

--- front_end/src/utils/booking.test.js
+++ front_end/src/utils/booking.test.js
@@ -3,7 +3,7 @@ import { getBookingTimeError } from './booking';
 
 describe('getBookingTimeError', () => {
   test('returns a clear error when duration is shorter than 3 hours', () => {
-    const error = getBookingTimeError('2026-07-01T08:00', '2026-07-01T10:00');
+    const error = getBookingTimeError('2026-07er-01T08:00', '2026-07-01T10:00');
 
     expect(error).toBe('Thời gian thuê tối thiểu là 3 tiếng');
   });
